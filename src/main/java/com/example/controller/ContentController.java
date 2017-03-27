3b279c37c9cf0fff48d877a93f2006c11306c3dd
zx17 <1293933511@qq.com>
2017-03-27 15:57:41 +0800

    评论
    添加评论功能

--- src/main/java/com/example/controller/ContentController.java
+++ src/main/java/com/example/controller/ContentController.java
@@ -1,14 +1,13 @@
 package com.example.controller;
 
-import com.example.Entity.Contentlist;
-import com.example.Mapper.ContentMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.Entity.User;
+import com.example.Util.SessionUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
-
 
 
 /**
@@ -16,5 +15,10 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 public class ContentController {
-
+    @RequestMapping(value = "/ss",method = RequestMethod.GET)
+    public String say(HttpServletRequest request,Model model){
+        User u= SessionUtil.getLoginUser(request);
+        model.addAttribute("username",u.getName());
+        return "content";
+    }
 }
